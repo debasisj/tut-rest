27349c86fef3fc2593e5df3d3d8f62253ae128a1
Deb Jagadeba <deb.jagadev@live.com>
2025-07-10 12:15:24 +1000

    added cross origin

--- rest/src/main/java/payroll/EmployeeController.java
+++ rest/src/main/java/payroll/EmployeeController.java
@@ -7,17 +7,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // tag::hateoas-imports[]
 // end::hateoas-imports[]
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 class EmployeeController {
 
